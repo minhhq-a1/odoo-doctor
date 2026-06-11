10ae1b625a2f6da3960997177e324cd3438e55c5
Odoo Doctor <odoo-doctor@dev.local>
2026-06-11 12:23:13 +0700

    feat(rules): add record-rule-without-domain

--- src/odoo_doctor/cli/app.py
+++ src/odoo_doctor/cli/app.py
@@ -29,6 +29,7 @@ import odoo_doctor.rules.security.missing_access_csv  # noqa: F401
 import odoo_doctor.rules.security.unknown_model_in_access_csv  # noqa: F401
 import odoo_doctor.rules.security.raw_sql_interpolation  # noqa: F401
 import odoo_doctor.rules.security.public_controller_sudo  # noqa: F401
+import odoo_doctor.rules.security.record_rule_without_domain  # noqa: F401
 import odoo_doctor.rules.xml.duplicate_xml_id  # noqa: F401
 import odoo_doctor.rules.xml.missing_xml_ref  # noqa: F401
 import odoo_doctor.rules.xml.view_field_not_in_model  # noqa: F401
