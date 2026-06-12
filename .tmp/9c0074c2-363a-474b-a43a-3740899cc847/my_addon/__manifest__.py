c26bceda3ed854494b2dd4f3f11bd16a0a279fa8
Odoo Doctor <odoo-doctor@dev.local>
2026-06-12 17:45:31 +0700

    chore: remove accidentally tracked temporary test files

--- .tmp/9c0074c2-363a-474b-a43a-3740899cc847/my_addon/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'My Addon',
-    'version': '17.0.1.0.0',
-    'depends': ['base'],
-    'data': ['security/ir.model.access.csv', 'views/v.xml'],
-    'installable': True,
-    'license': 'LGPL-3',
-}
